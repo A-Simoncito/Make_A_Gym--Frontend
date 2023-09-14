42687d1201bca41cde61cad91e02de177202a6c0
Simon Escudero <as.escudero@alumno.etec.um.edu.ar>
2023-09-14 20:36:53 -0300

    carrito2.1

--- make-a-gym/src/App.jsx
+++ make-a-gym/src/App.jsx
@@ -4,7 +4,6 @@ import Login from './Components/Login/login.jsx';
 import Register from './Components/Register/register.jsx';
 import Cart from './Components/Cart/CartContent.jsx';
 import DataProvider from './Components/Context/DataContext'
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
