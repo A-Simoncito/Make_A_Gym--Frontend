21c66823bafb1ac15ffa75920b9ae20742b0ed5b
Simon Escudero <as.escudero@alumno.etec.um.edu.ar>
2023-09-19 11:56:08 -0300

    cambios19

--- make-a-gym/src/Components/Body/body.jsx
+++ make-a-gym/src/Components/Body/body.jsx
@@ -7,127 +7,51 @@ import pecho5 from '../../multimedia/pecho5.jpg'
 import pierna1 from '../../multimedia/pierna1.jpg'
 import pierna4 from '../../multimedia/pierna4.jpg'
 import pierna5 from '../../multimedia/pierna5.jpg'
-
-
-
-
-
-
+import { Maquina } from '../Models/maquina'
 import './body.css';
 
 export default function Body() {
 
-    
-  return (
-    <body>
-        <br /><br />
-        <h1 class="MaPecho">Maquinas de Pecho, Espalda y Piernas</h1>
 
-        <br /><br /><br />
-        <div class="Container-Items">
-            <div class="item">
-                <figure>
-                    <img src={pecho1} alt="producto"/>
-                </figure>
+    return (
+        <body>
+            <br /><br />
+            <h1 class="MaPecho">Maquinas de Pecho, Espalda y Piernas</h1>
 
-                <div class="info-producto">
-                    <h2>Press de Banca stronger</h2>
-                    <p class="Precio">$440</p>
-                    <button class ="btn-add-cart">Añadir al Carrito</button>
-                </div>
+            <br /><br /><br />
+            <div class="Container-Items">
+
+                {buildMachineCard(new Maquina('Press de banca stronger', 440, pecho1))}
+                {buildMachineCard(new Maquina('Press de banca ', 180, pecho4))}
+                {buildMachineCard(new Maquina('Press de banca inclinado', 320, pecho5))}
+                {buildMachineCard(new Maquina('Multifuncion espalda', 640, espalda1))}
+                {buildMachineCard(new Maquina('Dorsalera', 315, espalda3))}
+                {buildMachineCard(new Maquina('Espalda Inferior-Hombros', 455, espalda4))}
+                {buildMachineCard(new Maquina('Prensa 45°', 440, pierna1))}
+                {buildMachineCard(new Maquina('Sillon Cuadriceps', 120, pierna4))}
+                {buildMachineCard(new Maquina('Camilla inclinable', 200, pierna5))}
+
             </div>
-            <div class="item">
-                <figure>
-                    <img src={pecho4} alt="producto"/>
-                </figure>
-                <div class="info-producto">
-                    <h2>Press de Banca</h2>
-                    <p class="Precio">$180</p>
-                    <button class ="btn-add-cart">Añadir al Carrito</button>
-                </div>
-                </div> <div class="item">
-                    <figure>
-                        <img src={pecho5} alt="producto"/>
-                    </figure>
-                    <div class="info-producto">
-                        <h2>Press de Banca inclinado</h2>
-                        <p class="Precio">$320</p>
-                        <button class ="btn-add-cart">Añadir al Carrito</button>
-                    </div>
-                </div>  
-        
+            
+            <script src="index.js">
+            </script>
+        </body>
+    )
+}
 
-
-                <div class="item">
-                    <figure>
-                        <img src={espalda1} alt="producto" />
-                    </figure>
-                    <div class="info-producto">
-                        <h2>Multifuncion Espalda</h2>
-                        <p class="Precio">$640</p>
-                        <button class ="btn-add-cart">Añadir al Carrito</button>
-                </div>
-            </div> 
-                <div class="item">
-                    <figure>
-                        <img src={espalda3} alt="producto"/>
-                    </figure>
-                    <div class="info-producto">
-                        <h2>Dorsalera</h2>
-                        <p class="Precio">$315</p>
-                        <button class ="btn-add-cart">Añadir al Carrito</button>
-                </div>
-            </div> 
-                <div class="item">
-                    <figure>
-                        <img src={espalda4} alt="producto" />
-                    </figure>
-                    <div class="info-producto">
-                        <h2>Espalda Inferior-Hombros</h2>
-                        <p class="Precio">$455</p>
-                        <button class ="btn-add-cart">Añadir al Carrito</button>
-                </div>
-            </div> 
-        
-
-
-
-            <div class="item">
-                <figure>
-                    <img src={pierna1} alt="producto" />
-                </figure>
-                <div class="info-producto">
-                    <h2>Prensa 45°</h2>
-                    <p class="Precio">$660</p>
-                    <button class ="btn-add-cart">Añadir al Carrito</button>
-                </div>
-            </div> 
-            <div class="item">
-                <figure>
-                    <img src={pierna4} alt="producto" />
-                </figure>
-                <div class="info-producto">
-                    <h2>Sillon Cuadriceps</h2>
-                    <p class="Precio">$120</p>
-                    <button class ="btn-add-cart">Añadir al Carrito</button>
-                </div>
-            </div> 
-            <div class="item">
-                <figure>
-                    <img src={pierna5} alt="producto" />
-                </figure>
-                <div class="info-producto">
-                    <h2>Camilla inclinable</h2>
-                    <p class="Precio">$200</p>
-                    <button class ="btn-add-cart">Añadir al Carrito</button>
-                </div>
-            </div> 
+const buildMachineCard = (machine = new Maquina()) => {
+    return (
+        <div class="item">
+            <figure>
+                <img src={machine.img} alt="producto" />
+            </figure>
+            <div class="info-producto">
+                <h2>{machine.nombre}</h2>
+                <p class="Precio">${machine.precio}</p>
+                <button class="btn-add-cart" onClick={() => {
+                    console.log(machine)
+                }}>Añadir al Carrito</button>
+            </div>
         </div>
-    
-        <script src="index.js">
-
-        </script>
-        
-    </body>
     )
 }
---
+++ make-a-gym/src/Components/Models/maquina.js
@@ -0,0 +1,16 @@
+
+
+
+export class Maquina {
+
+    nombre
+    precio
+    img
+
+    constructor (nombre = '', precio = 0, img = '', ){
+        this.nombre = nombre
+        this.precio = precio
+        this.img = img
+    }
+}
+
