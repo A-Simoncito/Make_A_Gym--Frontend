4603cf957cc58c52363e744c31e38162e81c71cc
simon <as.escudero@alumno.etec.um.edu.ar>
2024-03-11 10:36:10 -0300

    barra de busqueda

--- make-a-gym/src/Components/Header/header.jsx
+++ make-a-gym/src/Components/Header/header.jsx
@@ -1,32 +1,30 @@
 import './header.css';
-import loginIcon from '../../multimedia/login.jpg'
-import {Link} from 'react-router-dom'
+import loginIcon from '../../multimedia/login.jpg';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header>
+      <div className="search-container">
+        <input type="text" placeholder="Buscar..." />
+        <button type="submit">Buscar</button>
+      </div>
 
-      
       <h1 className="TittleP">
-        <Link to ="/">Make a Gym</Link>
+        <Link to="/">Make a Gym</Link>
       </h1>
-     
 
-
-      <div class="login-icon">
-        <Link to= "/Login">
-          <img src={loginIcon} alt="login-icon"/>
+      <div className="login-icon">
+        <Link to="/Login">
+          <img src={loginIcon} alt="login-icon" />
         </Link>
-
-      </div>
-    
-      <div className = 'nav-container'>
-      <Link to= "/Cart">
-        <h1 className= 'seeCarrito'>🛒</h1>
-      </Link>
       </div>
 
-  
+      <div className="nav-container">
+        <Link to="/Cart">
+          <h1 className="seeCarrito">🛒</h1>
+        </Link>
+      </div>
     </header>
-  )
+  );
 }
